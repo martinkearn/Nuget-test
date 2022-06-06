fb3de0d08ebeae27b2e3a69439b0f8aaa3944b69
Martin Kearn <martin.kearn@microsoft.com>
2022-06-06 14:50:06 +0100

    Consuming package with service

--- consuming-project/Program.cs
+++ consuming-project/Program.cs
@@ -1,3 +1,6 @@
+using MartinKearnTestNuget.Interfaces;
+using MartinKearnTestNuget.Services;
+
 namespace SimpleWebApi
 {
     public class Program
@@ -12,6 +15,7 @@ namespace SimpleWebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<IBookService, BookService>();
 
             var app = builder.Build();
 
