18d7bf022003fe1f53da535d2ce9c5721fde4d86
Martin Kearn <martin.kearn@microsoft.com>
2022-06-06 14:22:34 +0100

    Add book service

---
+++ package-project/Services/BookService.cs
@@ -0,0 +1,24 @@
+﻿using MartinKearnTestNuget.Interfaces;
+
+namespace MartinKearnTestNuget.Services
+{
+    public class BookService : IBookService
+    {
+        public string GenerateBookTitle()
+        {
+            Random rand = new();
+            int stringlen = rand.Next(4, 10);
+            int randValue;
+            string str = "";
+            char letter;
+            for (int i = 0; i < stringlen; i++)
+            {
+                randValue = rand.Next(0, 26);
+                letter = Convert.ToChar(randValue + 65);
+                str += letter;
+            }
+
+            return str;
+        }
+    }
+}
